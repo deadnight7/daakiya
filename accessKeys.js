d1e1b24626aa620f45929ffcee3ae23801ffab19
Chirag Pandit <chirag.pandit@here.com>
2019-06-15 11:53:09 +0530

    Adding changes see description for more
    - Added cloud functions
    - Separated accessKeys
    - Completed Validation of addOrder and Add Delivery Associate

---
+++ accessKeys.js
@@ -0,0 +1,22 @@
+
+var module_firebaseConfig = {
+  apiKey: "AIzaSyBFE7JabWokLVa3MxZ-lvon0_-Eu-l4eHQ",
+  authDomain: "a-positronium.firebaseapp.com",
+  databaseURL: "https://a-positronium.firebaseio.com",
+  projectId: "a-positronium",
+  storageBucket: "a-positronium.appspot.com",
+  messagingSenderId: "284632398484",
+  appId: "1:284632398484:web:e3528a21d6e70c94"
+};
+
+var module_HERETrackingAccount = {
+  trackingEmail : "cpandit203@gmail.com",
+  trackingPassword : "Q2hpcmFnITEyMw==", //btoa("passwordString")
+  trackingAppId : "RoARPsgpf2jnpAT6n7zY"//Goto App Tracking and Create first device - navigate to Here Developer Plans and get your app ID
+};
+
+var module_HEREDeveloperAccess = {
+  trackingEmail : "cpandit203@gmail.com",
+  app_id: 'st58tn5HdBdzSlMNw5OX',
+  app_code: '53DQ_ew0UvU1KmWtUJQDYQ'
+};
